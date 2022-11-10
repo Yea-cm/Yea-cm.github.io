e6f5a8ca81c5c1179a10c682ae1370c5702a9bcc
Yea-cm <114003204+Yea-cm@users.noreply.github.com>
2022-11-10 08:31:06 -0500

    delete

--- Proyecto 7/js/nav.js
+++
@@ -1,9 +0,0 @@
-var nav = document.querySelector('nav');
-
-      window.addEventListener('scroll', function () {
-        if (window.pageYOffset > 100) {
-          nav.classList.add('alert-danger', 'shadow');
-        } else {
-          nav.classList.remove('alert-danger', 'shadow');
-        }
-      });
